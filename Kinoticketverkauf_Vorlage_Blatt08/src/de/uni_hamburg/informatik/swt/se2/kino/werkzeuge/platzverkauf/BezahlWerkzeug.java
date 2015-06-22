ae6244e67498ece4d81fafd7961f19cd052486c5
Utz Pöhlmann <4poehlma@informatik.uni-hamburg.de>
2015-06-22 17:13:26 +0200

    Die Ui Repariert

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/BezahlWerkzeug.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/BezahlWerkzeug.java
@@ -1,7 +1,5 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.platzverkauf;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Set;
 
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Platz;
@@ -9,7 +7,7 @@ import de.uni_hamburg.informatik.swt.se2.kino.materialien.Vorstellung;
 
 public class BezahlWerkzeug
 {
-    
+
     private BezahlWerkzeugUI _ui;
     private int _anzahlPlaetze;
     private Vorstellung _vorstellung;
@@ -17,7 +15,7 @@ public class BezahlWerkzeug
 
     public BezahlWerkzeug()
     {
-    	
+
     }
 
     public void setzePlaetze(Set<Platz> plaetze, Vorstellung vorstellung)
@@ -26,12 +24,12 @@ public class BezahlWerkzeug
         _vorstellung = vorstellung;
         berechneNeuenPreis();
     }
-    
+
     private void berechneNeuenPreis()
     {
-        _preis = _anzahlPlaetze * _vorstellung.getPreis(); 
+        _preis = _anzahlPlaetze * _vorstellung.getPreis();
     }
-    
+
     public boolean wurdeBezahlt()
     {
         return _ui.wurdeBezahlt();
@@ -39,21 +37,25 @@ public class BezahlWerkzeug
 
     public void bezahle()
     {
-        _ui = new BezahlWerkzeugUI(_preis, getVorstellungsDaten(), _anzahlPlaetze);
+        _ui = new BezahlWerkzeugUI(_preis, getVorstellungsDaten(),
+                _anzahlPlaetze);
     }
-    
+
     private String getVorstellungsDaten()
     {
-    	return (
-    			_vorstellung.getFilm().toString().toUpperCase() 
-    		   + " - "
-    		   + _vorstellung.getDatum()
-    		   + " (Saal "
-    		   + _vorstellung.getKinosaal()
-    		   + ": "
-    		   + _vorstellung.getAnfangszeit()
-    		   + ")"
-    	);
+        return ("["
+                + _vorstellung.getDatum().getTag()
+                + "."
+                + _vorstellung.getDatum().getMonat()
+                + "."
+                + _vorstellung.getDatum().getJahr()
+                + "] - "
+                + _vorstellung.getFilm().toString().toUpperCase().substring(12)
+                + " ("
+                + _vorstellung.getKinosaal().toString().substring(15)
+                + ": "
+                + _vorstellung.getAnfangszeit()
+                + ")");
     }
-    
+
 }
--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/BezahlWerkzeugUI.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/BezahlWerkzeugUI.java
@@ -2,203 +2,212 @@ package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.platzverkauf;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 
-import de.uni_hamburg.informatik.swt.se2.kino.materialien.Vorstellung;
-
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
 
 public class BezahlWerkzeugUI extends JDialog
 {
 
-    private JButton _okButton;
-    private JButton _abbrechenButton;
-    private JTextField _eingabeFeld;
-    private JLabel _summeAnzeiger;
-    private JLabel _restbetragAnzeiger;
-    private JLabel _eingabeFeldText;
-    private JLabel _summeAnzeigerText;
-    private JLabel _restbetragAnzeigerText;
-    private JPanel _knoepfe;
-    private JPanel _textFelder;
-    private boolean _istOKButton;
-    
     private JPanel _vorangaben;
     private JLabel _vorstellungsText;
     private JLabel _anzahlPlaetzeText;
-    
+
+    private JPanel _textFelder;
+    private JLabel _summeAnzeigerText;
+    private JLabel _summeAnzeiger;
+    private JLabel _eingabeFeldText;
+    private JTextField _eingabeFeld;
+    private JLabel _restbetragAnzeigerText;
+    private JLabel _restbetragAnzeiger;
+
+    private JPanel _knoepfe;
+    private JButton _okButton;
+    private JButton _abbrechenButton;
+
+    private boolean _istOKButton;
+
     private String _vorstellungsDaten;
     private int _anzahlPlaetze;
     private int _preis;
 
-    public BezahlWerkzeugUI(int preis, String vorstellungsDaten, int anzahlPlaetze)
+    private static final String TITEL = "SE2-Bezahlungsansicht SoSe 2015";
+    private JFrame _frame;
+
+    private float _eingabe;
+
+    public BezahlWerkzeugUI(int preis, String vorstellungsDaten,
+            int anzahlPlaetze)
     {
         _preis = preis;
         _vorstellungsDaten = vorstellungsDaten;
         _anzahlPlaetze = anzahlPlaetze;
-        
+
         _istOKButton = false;
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.setLayout(new BorderLayout());
+
         setupGUI();
-        this.setModal(true); //blockt das aufrufende Fenster
-        this.pack();
-        this.setVisible(true);
     }
-    
-    public BezahlWerkzeugUI()
-    { }
 
     private void setupGUI()
     {
-        setupFachwerte();
-        setupGrid();
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        //        _frame = new JFrame(TITEL);
+        //        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //        _frame.getContentPane().setLayout(new BorderLayout());
+        //        JComponent rightPanel = platzVerkaufsPanel;
+        //        JComponent bottomPanel = erstelleBeendenPanel();
+        //        JSplitPane splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+        //                leftPanel, rightPanel);
+        //        _frame.getContentPane().add(splitter, BorderLayout.CENTER);
+        //        _frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        //        _frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+
+        setupVorangaben();
+        setupTextFelder();
         setupKnoepfe();
+
+        setModal(true); // blockt das aufrufende Fenster
+        pack();
+        setResizable(false);
         setVisible(true);
+
+        Dimension d = Toolkit.getDefaultToolkit()
+            .getScreenSize();
+        int x = (d.width - getSize().width) / 2;
+        int y = (d.height - getSize().height) / 2;
+        setLocation(x - (this.getWidth() / 2), y - (this.getHeight() / 2));
     }
 
-    private void setupFachwerte()
-    {
-        setupVorangaben();
-        setupVorstellungText();
-        setupAnzahlPlaetzeText();
-    }
-    
     private void setupVorangaben()
     {
         _vorangaben = new JPanel(new FlowLayout());
-        
-    }
-    
-    private void setupVorstellungText()
-    {
+        _vorangaben.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
         _vorstellungsText = new JLabel();
         _vorstellungsText.setText(_vorstellungsDaten);
-        _vorstellungsText.setToolTipText("[Datum] FILM (Saal x: Zeit)");
-        _vorangaben.add(this);
-    }
-    
-    private void setupAnzahlPlaetzeText()
-    {
+        _vorstellungsText.setToolTipText("[Datum] - FILM (Saal x: Zeit)");
+        _vorangaben.add(_vorstellungsText);
+
         _anzahlPlaetzeText = new JLabel();
         _anzahlPlaetzeText.setText("Ausgewählte Plätze: " + _anzahlPlaetze);
         _anzahlPlaetzeText.setToolTipText("Zeigt die Anzahl der aktuell ausgewählten Plätze");
+        _vorangaben.add(_anzahlPlaetzeText);
+
+        _vorangaben.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0,
+                Color.black));
+        this.add(_vorangaben, BorderLayout.PAGE_START);
+
     }
-    
-    private void setupGrid()
+
+    private void setupTextFelder()
     {
-        setupTextFelderJPanel();
-        setupFelder();
-        setupEingabeWerte();
+
+        _textFelder = new JPanel(new GridLayout(3, 2));
+
+        _summeAnzeigerText = new JLabel();
+        _summeAnzeigerText.setText("Zu zahlen (In €):");
+        _summeAnzeigerText.setToolTipText("Zeigt den addierten Preis aller gewählten Sitzplätze an.");
+        _textFelder.add(_summeAnzeigerText);
+
+        _summeAnzeiger = new JLabel();
+        _summeAnzeiger.setText((_preis / 100) + "," + (_preis % 100) + "");
+        _summeAnzeiger.setToolTipText("Zeigt den addierten Preis aller gewählten Sitzplätze an.");
+        _textFelder.add(_summeAnzeiger);
+
+        _eingabeFeldText = new JLabel();
+        _eingabeFeldText.setText("Gegeben (In €):");
+        _eingabeFeldText.setToolTipText("Hier eingeben, was der Kunde in Bar gegeben hat.");
+        _textFelder.add(_eingabeFeldText);
+
+        _eingabeFeld = new JTextField();
+        _eingabeFeld.setText("");
+        _eingabeFeld.setToolTipText("Hier eingeben, was der Kunde in Bar gegeben hat.");
+        _eingabeFeld.addKeyListener(new KeyListener()
+        {
+
+            @Override
+            public void keyTyped(KeyEvent e)
+            {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e)
+            {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e)
+            {
+                try
+                {
+                    do
+                    {
+                        _eingabe = Float.parseFloat(_eingabeFeld.getText());
+                        aktualisiereRestbetragAnzeiger();
+                    }
+                    while (_eingabeFeld.getText()
+                        .equals("(\\s+\\d+([,\\.]\\d\\d?)?\\s+)"));
+                }
+                catch (NumberFormatException e1)
+                {
+                    fehler();
+                }
+
+            }
+        });
+        _textFelder.add(_eingabeFeld);
+
+        _restbetragAnzeigerText = new JLabel();
+        _restbetragAnzeigerText.setText("Rückgeld (In €):");
+        _restbetragAnzeigerText.setToolTipText("Zeigt den Saldo der Kasse an.");
+        _textFelder.add(_restbetragAnzeigerText);
+
+        _restbetragAnzeiger = new JLabel();
+        _restbetragAnzeiger.setText("");
+        _restbetragAnzeiger.setToolTipText("Zeigt den Saldo der Kasse an.");
+        _textFelder.add(_restbetragAnzeiger);
+
+        this.add(_textFelder, BorderLayout.CENTER);
+
+    }
+
+    private void fehler()
+    {
+        JOptionPane error = new JOptionPane(JOptionPane.OK_OPTION);
+        error.showMessageDialog(this, "Bitte nur Zahlen eingeben.", "Warnung",
+                JOptionPane.WARNING_MESSAGE);
+        error.setVisible(true);
     }
 
     private void setupKnoepfe()
     {
-        setupKnoepfeJPanel();
-        setupOKButton();
-        setupAbbrechenButton();
-    }
-
-    private void setupEingabeWerte()
-    {
-        setupEingabeFeld();
-        setupSummenAzeiger();
-        setupRestbetragAnzeiger();
-    }
-
-    private void setupFelder()
-    {
-        setupEingabeFeldText();
-        setupSummenAzeigerText();
-        setupRestbetragAnzeigerText();
-    }
-
-    private void setupTextFelderJPanel()
-    {
-        _textFelder = new JPanel(new GridLayout(2,3));
-    }
-
-    private void setupKnoepfeJPanel()
-    {
         _knoepfe = new JPanel(new FlowLayout());
-    }
+        _knoepfe.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
-    private void setupRestbetragAnzeigerText()
-    {
-        _restbetragAnzeigerText = new JLabel();
-        _restbetragAnzeigerText.setText("Rückgeld:");
-        _restbetragAnzeigerText.setToolTipText("Zeigt den Saldo der Kasse an.");
-    }
-
-    private void setupSummenAzeigerText()
-    {
-        _summeAnzeigerText = new JLabel();
-        _summeAnzeigerText.setText("Zu zahlen:");
-        _summeAnzeigerText.setToolTipText("Zeigt den addierten Preis aller gewählten Sitzplätze an.");
-    }
-
-    private void setupEingabeFeldText()
-    {
-        _eingabeFeldText = new JLabel();
-        _eingabeFeldText.setText("Gegeben (BAR):");
-        _eingabeFeldText.setToolTipText("Hier eingeben, was der Kunde in Bar gegeben hat.");
-    }
-
-    private void setupRestbetragAnzeiger()
-    {
-        _restbetragAnzeigerText = new JLabel();
-    }
-
-    private void setupSummenAzeiger()
-    {
-        _summeAnzeiger = new JLabel();
-    }
-
-    private void setupEingabeFeld()
-    {
-        _eingabeFeld = new JTextField();
-        
-        _eingabeFeld.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-//            	_restbetragAnzeiger._____________();
-            }
-        });
-
-    }
-
-    private void setupAbbrechenButton()
-    {
-        _abbrechenButton = new JButton();
-        _abbrechenButton.setText("ABBRECHEN");
-        _abbrechenButton.setToolTipText("Bricht den Bezahlprozess ab.");
-        _abbrechenButton.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                schliessen();
-            }
-        });
-        _knoepfe.add(this);
-    }
-
-    private void schliessen()
-    {
-        this.dispose();
-    }
-
-    private void setupOKButton()
-    {
         _okButton = new JButton();
         _okButton.setText("OK");
         _okButton.setToolTipText("Führt den Bezahlvorgang aus.");
@@ -210,15 +219,41 @@ public class BezahlWerkzeugUI extends JDialog
                 bestaetigen();
             }
         });
-        _knoepfe.add(this);
+        _knoepfe.add(_okButton);
+
+        _abbrechenButton = new JButton();
+        _abbrechenButton.setText("ABBRECHEN");
+        _abbrechenButton.setToolTipText("Bricht den Bezahlprozess ab.");
+        _abbrechenButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                schliessen();
+            }
+        });
+        _knoepfe.add(_abbrechenButton);
+
+        this.add(_knoepfe, BorderLayout.PAGE_END);
     }
-    
+
+    private void aktualisiereRestbetragAnzeiger()
+    {
+        _restbetragAnzeiger.setText(Integer.parseInt(_summeAnzeiger.getText())
+                - _eingabe + "");
+    }
+
+    private void schliessen()
+    {
+        this.dispose();
+    }
+
     private void bestaetigen()
     {
         _istOKButton = true;
         schliessen();
     }
-    
+
     public boolean wurdeBezahlt()
     {
         return _istOKButton;
